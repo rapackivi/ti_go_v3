9bac0981ebab22e1f49d6ecb261133f5fc5b35ae
rapackivi <localit1981@gmail.com>
2021-02-22 09:04:25 +0200

    app boilerplate

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser')
+const passport = require('passport');
+const mongoose = require('mongoose');
+const path = require('path')
 
-const authRouter = require('./server/routes/auth')
+mongoose.connect(require('./config/keys').mongoURI, 
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true  
+  }, (err) => console.log(err||"DB connect!"));
+const app = express();
+require('./middleware/passport')(passport);
+//using passport for authentificate
+app.use(passport.initialize());
 
-app.use('/api/auth', authRouter)
 
-module.exports = app
+//parsing of request body
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+//logging of api requests
+app.use(require('morgan')("dev"))
+app.use(require('cors')())
+//connect static
+app.use('/uploads', express.static('uploads'))
+//add all routes
+app.use("/api/auth", require('./routes/auth'));
+app.use("/api/analytics", require('./routes/analytics'));
+app.use("/api/category", require('./routes/category'));
+app.use("/api/position", require('./routes/position'));
+app.use("/api/order", require('./routes/order'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/dist/client'))
+  app.get('*', (req, res)=>{
+    res.sendFile(
+      path.resolve(
+        __dirname, 'client', 'dist', 'client', 'index.html'
+      )
+    )
+  })
+}
+module.exports = app;
